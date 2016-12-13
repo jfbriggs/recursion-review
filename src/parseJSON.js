ea167e05723282936b82c7161bc0cc4137ff4ec9
Hack Reactor Students <accounts@hackreactor.com>
2016-12-12 20:48:09 -0800

    Add functionalty is not completely working

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,52 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  // Code for objects
+  if (json[0] === "{") {
+    if (json === "{}") {
+      return {};
+    }
+    var newObj = {};
+    var noBrackets = json.substring(1, json.length - 1);
+    noBrackets = noBrackets.replace(/ /g, '');
+    var stringPairs = noBrackets.split(","); // ==> ["'3':'s'","'b':'2'"]
+    stringPairs = stringPairs.map(function(element) {
+      return element.split(":");
+    });  // stringPairs => [['"3"', '"s"'], ['"b"', '"2"']]
+    for (var i = 0; i < stringPairs.length; i++) {
+      newObj[parseJSON(stringPairs[i][0])] = (stringPairs[i][1]);
+    }
+    for (var key in newObj) {
+      parseJSON(newObj[key]);
+    }
+    return newObj;
+  }
+
+  // Code for arrays
+  else if (json[0] === "[") {
+    if (json === "[]") {
+      return [];
+    }
+    var noBrackets = json.slice(1, json.length - 1);
+    noBrackets = noBrackets.replace(/ /gi, '');
+    var elementStrings = noBrackets.split(",");
+    for (var i = 0; i < elementStrings.length; i++) {
+      elementStrings[i] = parseJSON(elementStrings[i]);
+    }
+    return elementStrings;
+
+  // Code for basic values
+  } else if (json[0] == '"') {
+    return json.slice(1,json.length-1);
+  } else if (json === "true") {
+    return true;
+  } else if (json === "false") {
+    return false;
+  } else if (json === "null") {
+    return null;
+  } else {
+    return Number(json);
+  }
 };
+
